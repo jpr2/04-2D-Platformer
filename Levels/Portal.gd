1734ec9135b9092d061f5ca02201b22ee8b205d0
jpr2 <77981447+jpr2@users.noreply.github.com>
2021-03-23 15:35:16 -0400

    Completes Project 04

---
+++ Levels/Portal.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+func _ready():
+	pass
+
+
+func _on_Portal_entered(body):
+	if body.name == "Player":
+		if name == "Portal_to_2":
+			Global.level = 1
+		if name == "Portal_to_3":
+			Global.level = 2
+		if name == "Portal_to_4":
+			Global.level = 3
+		if Global.level < Global.levels.size():
+			get_tree().change_scene(Global.levels[Global.level])
+		else:
+			get_tree().change_scene("res://Levels/Game_Over.tscn")
